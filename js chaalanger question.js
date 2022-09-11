149d6c0dcd25a43412be83c47d1e3989cc501bc2
Ritik <ritikkhandelwal650@gmail.com>
2022-09-10 19:42:43 -0700

    pratice1

---
+++ js chaalanger question.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of strings as argument
+// Sort the array elements alphabetically
+// Return the result
+
+function myFunction(arr) {
+   
+            return(arr.sort()); 
+         }
+ const result = myFunction([2,1,6,5,4]);
+ console.log(result);

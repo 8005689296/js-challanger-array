b6a96f638dbb9d6c11d655d63beb39ae63912c1c
Ritik <ritikkhandelwal650@gmail.com>
2022-09-10 20:21:11 -0700

    pratice 5

---
+++ 5. pratice5.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+
+function myFunction(a) {
+            return(a[2]);
+}
+ const re = myFunction([1,2,3,4,5],2);
+console.log(re);
+
